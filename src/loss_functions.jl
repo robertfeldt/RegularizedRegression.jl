af667be4104d1044910bf2d5f9038b4c49c0be0a
Robert Feldt <robert.feldt@gmail.com>
2016-10-17 10:01:37 +0200

    added MASE loss/accuracy function

--- src/loss_functions.jl
+++ src/loss_functions.jl
@@ -7,6 +7,19 @@ medape(yhat, y) = 100.0 * median(ape(yhat, y))
 # Mean Absolute Percentage Error
 mape(yhat, y) = 100.0 * mean(ape(yhat, y))
 
+# Mean Absolute Scaled Error, see https://en.wikipedia.org/wiki/Mean_absolute_scaled_error
+# Note that it is not defined if the y vector is constant => all forecast errors are 0.0.
+function mase{N <: Number}(yhat::Vector{N}, y::Vector{N})
+    T = length(y)
+    @assert length(yhat) == T
+    sum_abs_errors = sumabs(yhat .- y) 
+    sum_abs_forecast_errors = 0.0
+    for t in 2:T
+        sum_abs_forecast_errors += abs(y[t] - y[t-1])
+    end
+    return sum_abs_errors / (T/(T-1) * sum_abs_forecast_errors)
+end
+
 # R2 is R squared which is how much more variation is explained by the 
 # fitted values (yhat) compared to just using the mean as the model?
 r2(yhat, y) = 1.0 - ( sum((y .- yhat).^2) / sum((y .- mean(y)).^2) )
--- test/test_loss_functions.jl
+++ test/test_loss_functions.jl
@@ -1,10 +1,24 @@
-using RegularizedRegression: mape
+using RegularizedRegression: ape, mape, medape, mase
 
 @testset "Loss functions" begin
-    
-@testset "MAPE" begin
+
+@testset "APE (Absolute Percentage Error)" begin
+    @test ape([1.5, 2.0], [1.0, 1.0]) == [0.50, 1.0]
+    @test ape([1.5, 2.0, 3.0], [1.0, 1.0, 1.0]) == [0.50, 1.0, 2.0]
+end
+
+@testset "MAPE (Mean Absolute Percentage Error)" begin
     @test mape([1.5, 2.0], [1.0, 1.0]) == mean([50.0, 100.0])
     @test mape([1.5, 2.0, 3.0], [1.0, 1.0, 1.0]) == mean([50.0, 100.0, 200.0])
+end
+
+@testset "MEDAPE (Median Absolute Percentage Error)" begin
+    @test medape([1.5, 2.0], [1.0, 1.0]) == median([50.0, 100.0])
+    @test medape([1.5, 2.0, 3.0], [1.0, 1.0, 1.0]) == median([50.0, 100.0, 200.0])
+end
+
+@testset "MASE (Mean Absolute Scaled Error)" begin
+    @test mase([1.5, 2.0, 3.0], [1.0, 1.5, 2.0]) == (0.5+0.5+1.0)/(3.0/2.0 * (0.5 + 0.5))
 end
 
 end
