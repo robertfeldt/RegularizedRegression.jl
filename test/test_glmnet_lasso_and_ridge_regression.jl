1c1ff51a15f73d3bc4d0c2097408a36e76ac2117
Robert Feldt <robert.feldt@gmail.com>
2016-10-19 13:18:44 +0200

    added ridge regression test

--- test/test_glmnet_lasso_and_ridge_regression.jl
+++ test/test_glmnet_lasso_and_ridge_regression.jl
@@ -5,3 +5,9 @@ using RegularizedRegression: LassoRegression, RidgeRegression, learn, coefficien
         test_learner(LassoRegression, 5.0) # Seems this needs to be higher for SLOPE than for SCAD and MCP
     end
 end
+
+@testset "Ridge regression (via GLMNet.jl)" begin
+    for _ in 1:NumTestRepetitions
+        test_learner(RidgeRegression, 5.0) # Seems this needs to be higher for SLOPE than for SCAD and MCP
+    end
+end
