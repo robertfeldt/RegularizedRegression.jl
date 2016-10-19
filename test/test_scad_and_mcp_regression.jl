46bb97e86a69bacfb87157239396b944a3a7d7a1
Robert Feldt <robert.feldt@gmail.com>
2016-10-19 10:18:50 +0200

    adjusted the constraints somewhat since SLOPE seems less robust

--- test/test_scad_and_mcp_regression.jl
+++ test/test_scad_and_mcp_regression.jl
@@ -2,12 +2,12 @@ using RegularizedRegression: ScadRegression, McpRegression
 
 @testset "SCAD regression" begin
     for _ in 1:NumTestRepetitions
-        test_learner_with_includeIntercept(ScadRegression)
+        test_learner_with_includeIntercept(ScadRegression, 5.0)
     end
 end
 
 @testset "MCP regression" begin
     for _ in 1:NumTestRepetitions
-        test_learner_with_includeIntercept(McpRegression)
+        test_learner_with_includeIntercept(McpRegression, 5.0)
     end
 end
--- test/test_slope_regression.jl
+++ test/test_slope_regression.jl
@@ -2,6 +2,6 @@ using RegularizedRegression: SlopeRegression, learn, coefficients
 
 @testset "Slope regression" begin
     for _ in 1:NumTestRepetitions
-        test_learner(SlopeRegression, 10.0)
+        test_learner(SlopeRegression, 15.0) # Seems this needs to be higher for SLOPE than for SCAD and MCP
     end
 end
